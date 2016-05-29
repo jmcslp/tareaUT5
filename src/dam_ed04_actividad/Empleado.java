289b4823c7c28362eab015854853b27d19b89827
jmcslp <jmcs@techie.com>
2016-05-29 03:08:03 +0100

    Commit 4
    Se añaden en clase Empleado los getters y setters necesarios.

--- src/dam_ed04_actividad/Empleado.java
+++ src/dam_ed04_actividad/Empleado.java
@@ -22,5 +22,28 @@ public class Empleado {
         this.apellidos = apellidos;
         this.dni = dni;
     }
-    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
 }
