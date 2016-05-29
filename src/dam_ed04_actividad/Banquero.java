1dfc275c37a1a10f89edd6e5aef701eedd49cd55
jmcslp <jmcs@techie.com>
2016-05-29 03:24:40 +0100

    Commit 6
    Se crea clase Banquero con atributos nombre, apellidos y dni. Tambien se crea contructores

---
+++ src/dam_ed04_actividad/Banquero.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dam_ed04_actividad;
+
+/**
+ *
+ * @author José Manuel Caballero Sosa
+ */
+public class Banquero {
+    
+    private String nombre;
+    private String apellidos;
+    private String dni;
+
+    public Banquero() {
+    }
+
+    public Banquero(String nombre, String dni) {
+        this.nombre = nombre;
+        this.dni = dni;
+    }
+    
+    
+}
