897a928a91ae11a5fef356d3a6746615c901b451
jmcslp <jmcs@techie.com>
2016-05-29 03:06:01 +0100

    Commit 4
     Creación de clase Empleado con los atributos y constructores.

---
+++ src/dam_ed04_actividad/Empleado.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dam_ed04_actividad;
+
+/**
+ *
+ * @author José Manuel Caballero Sosa
+ */
+public class Empleado {
+    private String nombre;
+    private String apellidos;
+    private String dni;
+
+    public Empleado() {
+    }
+
+    public Empleado(String nombre, String apellidos, String dni) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.dni = dni;
+    }
+    
+}
