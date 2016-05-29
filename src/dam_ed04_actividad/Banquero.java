53cf776806b4601bbd0fa12a1ec13ca67fc13c0a
jmcslp <jmcs@techie.com>
2016-05-29 03:26:29 +0100

    Commit 7
    Se añade a clase Banquero los getters y setters necesarios.

--- src/dam_ed04_actividad/Banquero.java
+++ src/dam_ed04_actividad/Banquero.java
@@ -22,6 +22,29 @@ public class Banquero {
         this.nombre = nombre;
         this.dni = dni;
     }
-    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
     
 }
